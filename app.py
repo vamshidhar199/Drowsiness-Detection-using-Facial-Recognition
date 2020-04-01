5b2baf66ccce8d307698b6bedf23663422879262
vamshidhar <vamshiredmi@gmail.com>
2020-04-01 19:35:17 +0530

    ok

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def index():
     return render_template('index3.html')
 
-
+#main algorith route
 @app.route('/index2', methods=['POST', 'GET'])
 def index2():
     # index2 = request.form
@@ -56,6 +56,58 @@ def index2():
     labe=['closed','opened']
     #return render_template('piechart.html',f=lines,title='Sleepy chart', max=0.01,set=zip(values, labels,colors))
     return render_template('charts.html', f=lines, title='Sleepy chart', set=zip(v,labe,colors),max=0.01, values=values, labels=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','...'],closed=closed,opened=opened,alcount=alcount)
+
+
+
+#secondary algorithm
+
+@app.route('/indexa2', methods=['POST', 'GET'])
+def indexa2():
+    # index2 = request.form
+    # return render_template('index2.html')
+    os.system('dir')
+    os.system('python3 Drowsiness_detection.py -a alarm.wav')
+    #v = functions.val()
+    # assert isinstance(v, str)
+    lines = []
+    values=[]
+    labels=[]
+    closed=0
+    opened=0
+    v=[0,0]
+    alcount=0
+    colors=['red','blue']
+    w = open('weeka.txt', 'a')
+    #legend = 'Monthly Data'
+    with open("texta.txt") as file:
+        for line in file:
+            line = line.strip()  # or some other preprocessing
+            lines.append(line)
+            if float(line)<0.3:
+                closed=closed+1
+            else:
+                opened=opened+1
+            # storing everything in memory!
+            values.append(float(line))
+            #values = values[1:50]
+            l = len(values)
+            k=0
+            for i in range(10):
+                labels.append(k)
+
+    with open("alcounta.txt") as al:
+        for l in al:
+            if l.startswith("on"):
+                alcount=alcount+1
+            #s="\n".join(lines)
+    w.write(str(alcount)+"\n")
+    #w.write("\n")
+    v[0]=closed
+    v[1]=opened
+    labe=['closed','opened']
+    #return render_template('piechart.html',f=lines,title='Sleepy chart', max=0.01,set=zip(values, labels,colors))
+    return render_template('charts.html', f=lines, title='Sleepy chart', set=zip(v,labe,colors),max=0.01, values=values, labels=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','...'],closed=closed,opened=opened,alcount=alcount)
+
 
 @app.route('/index4', methods=['POST', 'GET'])
 def index4():
